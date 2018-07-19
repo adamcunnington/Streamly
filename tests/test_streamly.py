eccddb589e7221c2ad9f98c03efba1c006f35458
Adam Cunnington <ac@adamcunnington.info>
2018-07-19 22:25:26 +0100

    Added another crappy test func - to test _chop

--- tests/test_streamly.py
+++ tests/test_streamly.py
@@ -3,6 +3,12 @@ import io
 import streamly
 
 
+def test_chop():
+    sequence = [1, 2, 3, 4, 5, 6, "foo", "bar"]
+    part1, part2 = streamly._chop(sequence, 4)
+    assert part1 == [1, 2, 3, 4] and part2 == [5, 6, "foo", "bar"]
+
+
 def test_stream():
     string_io = io.StringIO()
     stream = streamly.Stream(string_io, 0)
