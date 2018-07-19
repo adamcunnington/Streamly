fc14b7a7352f0c7126debae1fa213ece76ba8f22
Adam Cunnington <ac@adamcunnington.info>
2018-07-19 22:04:56 +0100

    wrote first test

---
+++ tests/test_streamly.py
@@ -0,0 +1,9 @@
+import io
+
+import streamly
+
+
+def test_stream():
+    string_io = io.StringIO
+    stream = streamly.Stream(string_io, 0)
+    assert stream.stream is string_io and stream.length == 0
