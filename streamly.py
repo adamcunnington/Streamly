7049fec464352e5a8e0a4ac1110a1ad47a8b6e63
Adam Cunnington <ac@adamcunnington.info>
2018-07-19 08:48:55 +0100

    Read function completed (untested)

--- streamly.py
+++ streamly.py
@@ -1,6 +1,9 @@
 import logging
 
 
+_MIN_READ_SIZE = 128
+
+
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 
 
@@ -18,6 +21,7 @@ class Streamly:
         self.streams = [{
             "stream": getattr(stream, "stream", stream),
             "header_found": False,
+            "header_row_found": False,
             "length": getattr(stream, "length", None),
             "footer_found": False,
             "bytes_read": 0
@@ -28,8 +32,9 @@ class Streamly:
         self.retain_first_header_row = retain_first_header_row
         self.current_stream_index = 0
         self.end_reached = False
-        self._tail = b""
-        self._head = b""
+        self._end_of_prev_read = b""
+        self._bytes_read_ahead = b""
+        self._bytes_backlog = b""
 
     @staticmethod
     def _read(stream, size):
@@ -59,7 +64,7 @@ class Streamly:
 
     @property
     def header_found(self):
-        return self.current_stream["header_found"]
+        return self.current_stream["header_found"] and self.current_stream["header_row_found"]
 
     @property
     def is_last_stream(self):
@@ -81,7 +86,7 @@ class Streamly:
     def _log_progress(self):
         pass
 
-    def _remove_footer(self, _bytes, max_size):
+    def _remove_footer(self, _bytes, max_size):  # bytes is a builtin name
         index = _bytes.find(self.footer_identifier)
         if index == -1:
             head = self._read(self.current_stream, len(self.footer_identifier) - 1)
@@ -93,7 +98,7 @@ class Streamly:
         self._end_stream()
         return _bytes[: index], b""
 
-    def _remove_header(self, _bytes):
+    def _remove_header(self, _bytes):  # bytes is a builtin name
         current_stream = self.current_stream
         start = None
         if not current_stream["header_found"]:
@@ -110,24 +115,43 @@ class Streamly:
         return (_bytes if index == 0 else _bytes[index: ]), b""
 
     def read(self, size=8192):
-        if self.end_reached:
+        if self.end_reached and not self._bytes_backlog:
+            # Signify to the caller that the stream is exhausted
             return b""
         bytes_to_return = b""
         total_size = 0
-        while (total_size < size) and not self.end_reached:
-            _bytes = b""
-            current_stream = self.current_stream
+        while total_size < size:
             size_remaining = size - total_size
-            size_to_read = max(size_remaining - len(self._head), 0)
-            if size_to_read:
-                _bytes = self._read(current_stream, size_to_read)
-            if not _bytes and not self._head:
-                self._end_stream()
-                continue
-            if self._header_check_needed() or self._tail:
-                _bytes, self._tail = self._remove_header(self._tail + _bytes, size_remaining)
-            if self._footer_check_needed():
-                _bytes = self._remove_footer(self._head + _bytes, size_remaining)
-            total_size += len(_bytes)
-            bytes_to_return += _bytes
+            if self._bytes_backlog:
+                processed_bytes = self._bytes_backlog[: size_remaining]
+                self._bytes_backlog = self._bytes_backlog[size_remaining: ]
+            elif self.end_reached:
+                break
+            else:
+                processed_bytes = b""
+                # If there are a small amount of bytes left to read, we could enter in to a situation where there are a
+                # large volumes of reads needed, i.e. perhaps we're still trying to identify where the header ends,
+                # constantly evaluating just 5 additional bytes at a time. Therefore, read at least the _MIN_READ_SIZE.
+                # Any bytes read ahead that won't be returned just now will be saved for a subsequent read.
+                raw_bytes = self._read(self.current_stream, max(size_remaining, _MIN_READ_SIZE))
+                # There's no point doing checks for the header and footer if there are no bytes left to read as the
+                # result will be the same as the last iteration of the loop.
+                if raw_bytes:
+                    if self._header_check_needed():
+                        self._end_of_prev_read, processed_bytes, self._bytes_read_ahead = self._remove_header(
+                            self._end_of_prev_read + raw_bytes, size_remaining)
+                    # Don't look for the footer unless the header is found. Can't do an else if as it might have only
+                    # just been found.
+                    if self.header_found and self._footer_check_needed():
+                        processed_bytes, self._bytes_read_ahead = self._remove_footer(
+                            self._bytes_read_ahead + raw_bytes, size_remaining)
+                else:
+                    self._end_stream()
+                    # We know that the stream we are reading from is exhausted but there could be data that was read
+                    # ahead in the previous footer check. We now know that this data can be returned but it could be
+                    # larger than the size_remaining and so, save it to a backlog that will be returned in subsequent
+                    # calls.
+                    self._bytes_backlog = self._bytes_read_ahead
+            total_size += len(processed_bytes)
+            bytes_to_return += processed_bytes
         return bytes_to_return
