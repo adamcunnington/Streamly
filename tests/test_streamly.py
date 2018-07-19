00dec525fc2a1f8ab2a43647721e62995eb9aaf5
Adam Cunnington <ac@adamcunnington.info>
2018-07-19 22:58:59 +0100

    Made test_chop more exhaustive

--- tests/test_streamly.py
+++ tests/test_streamly.py
@@ -5,21 +5,25 @@ import pytest
 import streamly
 
 
-@pytest.mark.parametrize("sequence, at_index", (
-    ([1, 2, 3, 4, 5, 6, "foo", "bar"], 4),
-    ([1, 2, 3, 4, 5, 6, "foo", "bar"], 0),
-    ([1, 2, 3, 4, 5, 6, "foo", "bar"], -0),
-    ([1, 2, 3, 4, 5, 6, "foo", "bar"], -1),
-    ([1, 2, 3, 4, 5, 6, "foo", "bar"], -4),
-    (["bar"], 4),
-    (["bar"], 0),
-    (["bar"], -0),
-    (["bar"], -1),
-    (["bar"], -4),
+@pytest.mark.parametrize("sequence, at_index, expected_part1, expected_part2", (
+    ([1, 2, 3, 4, 5, 6, "foo", "bar"], 4, [1, 2, 3, 4], [5, 6, "foo", "bar"]),
+    ([1, 2, 3, 4, 5, 6, "foo", "bar"], 0, [], [1, 2, 3, 4, 5, 6, "foo", "bar"]),
+    ([1, 2, 3, 4, 5, 6, "foo", "bar"], -0, [], [1, 2, 3, 4, 5, 6, "foo", "bar"]),
+    ([1, 2, 3, 4, 5, 6, "foo", "bar"], -1, [1, 2, 3, 4, 5, 6, "foo"], ["bar"]),
+    ([1, 2, 3, 4, 5, 6, "foo", "bar"], -4, [1, 2, 3, 4], [5, 6, "foo", "bar"]),
+    (["bar"], 4, ["bar"], []),
+    (["bar"], 0, [], ["bar"]),
+    (["bar"], -0, [], ["bar"]),
+    (["bar"], -1, [], ["bar"]),
+    (["bar"], -4, [], ["bar"]),
 ))
-def test_chop(sequence, at_index):
-    sequence = [1, 2, 3, 4, 5, 6, "foo", "bar"]
-    part1, part2 = streamly._chop(sequence, 4)
+def test_chop(sequence, at_index, expected_part1, expected_part2):
+    part1, part2 = streamly._chop(sequence, at_index)
+    if (at_index >= 0) and (at_index <= len(sequence)):
+        assert len(part1) == at_index
+        assert len(part2) == len(sequence) - at_index
+    assert part1 == expected_part1
+    assert part2 == expected_part2
     assert part1 + part2 == sequence
 
 
